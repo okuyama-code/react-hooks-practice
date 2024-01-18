17114ffb7375fab6fde5d9d78d988b28e7846462
okuyama <130431374+okuyama-code@users.noreply.github.com>
2024-01-18 12:23:42 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import { AuthProvider } from './context/AuthContext';
 import UserAuth from './components/userAuth/UserAuth';
+import Bank from './pages/bank/Bank';
 
 function App() {
   return (
-    // AuthProviderを一番外側に置く
     <AuthProvider>
       <div className="App">
-        <h2>#4 useContext</h2>
-        <UserAuth />
+        <h2>#5 useReducer</h2>
+        <Bank />
       </div>
     </AuthProvider>
   );
